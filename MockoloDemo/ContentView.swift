2f08db94c76184cc9e29e4262af445fa65133e87
Maxwell Elliott <maxwell.elliott@gotinder.com>
2020-03-20 12:13:45 -0700

    Initial Commit

---
+++ MockoloDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MockoloDemo
+//
+//  Created by Maxwell Elliott on 3/20/20.
+//  Copyright © 2020 Tinder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
